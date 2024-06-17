b6175f297a6dcaca3d4951c86664e058041caa80
Adrian Junge <adjun@web.de>
2024-06-17 17:39:20 -0400

    weather get specific data from api

--- app/weather/routes.py
+++ app/weather/routes.py
@@ -6,6 +6,7 @@ import requests
 from app.weather import constants
 import logging
 from geopy.geocoders import Nominatim
+import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -17,8 +18,15 @@ logger = logging.getLogger(__name__)
 
 FORECAST_API = 'https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}'
 CURRENT_API = 'https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}'
+WEATHER_ICON_URL = 'https://openweathermap.org/img/w/{}.png'
 API_KEY = os.getenv(constants.API_KEY_ENV_VAR, None)
 CITY = 'Karlsruhe'
+
+
+def timestamp_to_datetime(timestamp, timezone):
+    utc_dt = datetime.datetime.utcfromtimestamp(timestamp)
+    timezone_offset = datetime.timedelta(seconds=timezone)
+    return (utc_dt + timezone_offset).strftime('%H:%M:%S')
 
 
 def get_data(url, city, url_params=None):
@@ -32,7 +40,30 @@ def get_data(url, city, url_params=None):
     url = url.format(lat, long, API_KEY)
     weather_data = requests.get(url).json()
     logging.debug(f'[*] Got weather data for city {city}: {weather_data}')
-    return weather_data
+    time_zone = int(weather_data.get('city').get('timezone'))
+
+    specific_weather_data = {
+        constants.SUNRISE: timestamp_to_datetime(
+            weather_data.get('city').get('sunrise'), time_zone
+        ),
+        constants.SUNSET: timestamp_to_datetime(
+            weather_data.get('city').get('sunset'), time_zone
+        ),
+        constants.FORECASTS: [],
+    }
+    for forecast in weather_data.get('list'):
+        specific_weather_data.get(constants.FORECASTS).append({
+            constants.TEMP_MIN: forecast.get('main').get('temp_min'),
+            constants.TEMP_MAX: forecast.get('main').get('temp_max'),
+            constants.WEATHER_FORECAST_ID: forecast.get('weather')[0].get('id'),
+            constants.WEATHER_FORECAST_DESCRIPTION: forecast.get('weather')[0].get('description'),
+            constants.WEATHER_FORECAST_ICON: forecast.get('weather')[0].get('icon'),
+            constants.TEMP_FEELS_LIKE: forecast.get('main').get('feels_like'),
+            constants.PROP_PRECIPITATION: forecast.get('pop'),
+            constants.TEMP: forecast.get('main').get('temp'),
+        })
+
+    return specific_weather_data
 
 
 @bp.route('/get_coordinates', methods=['GET'])
