fb7678e52132780e11ca9f7d94b15d6fe76b2d93
Adrian Junge <adjun@web.de>
2024-06-24 13:03:12 +0200

    add favicon

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,3 +1,4 @@
+from flask import send_from_directory
 from app.main import bp
 from app.weather.routes import get_weather
 
@@ -5,3 +6,11 @@ from app.weather.routes import get_weather
 @bp.route('/', methods=['GET'])
 def index():
     return get_weather()
+
+
+@bp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        bp.root_path,
+        'favicon.ico', mimetype='image/vnd.microsoft.icon'
+    )
