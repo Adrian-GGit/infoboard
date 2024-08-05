fc3cd71262d00245b95629a1c825d349c8a474df
Adrian Junge <adjun@web.de>
2024-08-05 23:27:06 +0200

    edit styling and add js reloader

--- app/static/js/weather/weather.js
+++ app/static/js/weather/weather.js
@@ -1,17 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const container = document.querySelector('html');
-    const imagePath = `url('/static/img/weather/background/${currentWeatherDescription}.jpg')`;
-    container.style.backgroundImage = imagePath;
-});
-
-// refresh information every 5 minutes
+// refresh information every 30 seconds
 setTimeout(() => {
     window.location.reload();
-}, 300000);
-
-// slide in animation
-$(".forecast-item").each(function(index){
-    $(this).css({
-        'animation-delay' : 0.1 * (1+index) + 's'
-    });
-});
+}, 30000);
